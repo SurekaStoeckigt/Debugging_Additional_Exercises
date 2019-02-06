bf1cf9b118b28655622136bac21051cc802964b7
Student <student@Admins-MacBook-Pro-3.local>
2019-02-06 09:54:46 +0000

    fizzbuzz debug exercise

---
+++ broken-fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,20 @@
+class FizzBuzz
+
+  def is_divisible_by(number, divisor)
+    number % divisor == 0
+  end
+
+  def play(number)
+    return "fizzbuzz" if is_divisible_by(number, 15)
+    return "fizz" if is_divisible_by(number, 3)
+    return "buzz" if is_divisible_by(number, 5)
+    number
+  end
+
+end
+
+
+fizzbuzz = FizzBuzz.new
+(1..100).each do |number|
+  puts fizzbuzz.play(number)
+end
